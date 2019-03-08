2237eb38d35962aec9c62ae9f84289f4183fb2ac
huanghuisheng <654607176@qq.com>
2019-03-08 16:41:59 +0800

    客户端1

--- demo/src/main/java/iov/spring/test/Test.java
+++ demo/src/main/java/iov/spring/test/Test.java
@@ -11,26 +11,26 @@ public class Test {
 
 	
 	public static void main(String[] args) {
-		
+		dfsdfsafdsa蓝色的减肥的方式决定了合法哦配合
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"applicationContext.xml");  
-		  
+		  的沙发沙发啊法师法师法师法师打饭
 	        context.start();  
-	    
+	    放大书法书法上的发放
 	        SqlSessionTemplate sqlSession =  (SqlSessionTemplate) context.getBean("sqlSessionTemplate");  
 	        
 		Test test=new Test();
 		
-		
+		大方豆腐第三方阿三发生颠覆阿斯蒂芬撒旦法沙发沙发扶手
 		
 		
 		
 
 		String a=DeviceMapper.class.getName()+".selectByPrimaryKey";
 		System.out.println("name is ---"+sqlSession);
-		
+		法师法师的发生发生颠覆示范点收到发顺丰
 		  List   list=  sqlSession.selectList(a);
-		
+		范德萨分阿瑟阿萨德阿萨德打发打算地方发
 		  System.out.println("-----"+list.size());
 		  
 		  
